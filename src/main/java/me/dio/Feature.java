b7333ca9a0e55b4b9af4b859002f3d7e4bbf7342
Julia Nogueira Figueiredo <julianogueira814@gmail.com>
2023-10-22 18:32:27 -0300

    mapeando e criando perfil dev

---
+++ src/main/java/me/dio/Feature.java
@@ -0,0 +1,9 @@
+package me.dio;
+
+import jakarta.persistence.Entity;
+import me.dio.BaseItem;
+
+@Entity(name = "tb_feature")
+public class Feature extends BaseItem {
+
+}
---
+++ src/main/java/me/dio/News.java
@@ -0,0 +1,9 @@
+package me.dio;
+
+import jakarta.persistence.Entity;
+import me.dio.BaseItem;
+
+@Entity(name = "tb_news")
+public class News extends BaseItem {
+
+}
